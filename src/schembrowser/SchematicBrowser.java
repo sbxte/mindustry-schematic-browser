4c588f5ecd50238b81da14a5343113795c2ac863
SByte <72693226+StormyBytes@users.noreply.github.com>
2024-03-17 11:24:08 +0700

    Keybind (shift + <schematic_menu>)
    TODO: make it work for mobile maybe

--- src/schembrowser/SchematicBrowser.java
+++ src/schembrowser/SchematicBrowser.java
@@ -1,8 +1,12 @@
 package schembrowser;
 
+import arc.*;
 import arc.files.*;
+import arc.input.*;
+import arc.scene.event.*;
 import arc.util.*;
 import mindustry.gen.*;
+import mindustry.input.*;
 import mindustry.mod.*;
 import schembrowser.ui.*;
 
@@ -23,6 +27,22 @@ public class SchematicBrowser extends Mod {
         ui.schematics.buttons.button("@schematicbrowser", Icon.host, () -> {
             ui.schematics.hide();
             schematicBrowserDialog.show();
+        });
+
+        // Keybind
+        // Shift + <schematic menu>
+        Core.scene.addListener(new InputListener() { // TODO: Make it work for mobile?
+            @Override
+            public boolean keyDown(InputEvent event, KeyCode keycode) {
+                if (!state.isMenu() && !ui.chatfrag.shown() && !ui.schematics.isShown() && !ui.database.isShown()
+                        && !ui.consolefrag.shown() && !ui.content.isShown() && !Core.scene.hasKeyboard()
+                        && keycode == Core.keybinds.get(Binding.schematic_menu).key && Core.input.shift()
+                ) {
+                    ui.schematics.hide();
+                    schematicBrowserDialog.show();
+                }
+                return super.keyDown(event, keycode);
+            }
         });
     }
 }
